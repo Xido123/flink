9dbe5f4c029c22c74204a4343391a05992d41e5e
xido <1661480468@qq.com>
2020-09-30 22:03:26 +0800

    a

--- README.md
+++ README.md
@@ -2,3 +2,62 @@
 <<<<<<< HEAD
 ### faa
 ##  faa
+\#这是一个markdown练习文档
+***照片工会啊***
+*撒旦f*
+
+
+***
+* * *
+——————————
+~~圣安东尼奥~~
+<u>清华大学</u>
+<u>北京大学</u>
+
+ahuh[^asf],
+[^asf]:dajsfkajkdlsfjlaksdj kflajsdkl 
+
+删除线
+
+
+* tesst
+* tet
++ test
++ test
+- test
+- test
+1. sekect
+2. seletc
+
+1. 第一项
+- 第一项嵌套的第一个元素
+- 第一项嵌套的第二个元素
+2. 第二项
+-第二项嵌套的第一个元素
+-第二项嵌套的第二个元素》
+> 123
+> fdsf 
+> adfads 
+>wer
+>>wer
+
+
+    fdfkdskfl kdlskfldsaklf
+     sdf sd fdsf sd
+     f 
+     sdaf 
+     asdf
+      sad
+      f asd
+       f
+       asdf
+        sd
+        f 
+        asd
+         asd
+          
+ | wer | wer |
+ |:----|----:|
+ |e  | ew |
+           
+
--- coderun/src/redo/tree/brinytrue.java
+++ coderun/src/redo/tree/brinytrue.java
@@ -1,5 +1,7 @@
 package redo.tree;
 
+import java.util.Stack;
+
 public class brinytrue {
 /**
  * 二叉树的遍历练习
@@ -45,7 +47,48 @@ public class brinytrue {
         preOrderTraveral(head.rightChild);
 
     }
-
+    /**
+     * 二叉树的后序遍历
+     * 二叉树的后序遍历
+     */
+    public static void postOrderTraveral(Node head){
+        if(head == null){
+            return;
+        }
+        postOrderTraveral(head.leftChild);
+        postOrderTraveral(head.rightChild);
+        System.out.println(head.val+" ");
+    }
+    /**
+     * 二叉树的前中后序遍历的非递归实现
+     * 二叉树的 前中后序遍历的非递归实现
+     * 二叉树的前中后序遍历的非递归实现
+     * 1 首先申请一个新的栈 记为stack
+     * 2 声明一个节点treenode 让其指向node的节点
+     * 3 如果treenode 的不为空，将treenode 的值打印 并将treenode入栈
+     * 让后让treenode指向treenode的右节点
+     *
+     * 重复步骤3 知道treenode为空
+     *
+     * 然后出栈 让tree弄得
+     * 执行tree弄得
+     * 的有孩子 重复步骤3 直到stack为空
+     *
+     */
+    public static void preOrderTraceralWithStack(Node head){
+       Stack<Node> stack = new Stack<>();
+       Node cur = head;
+       stack.push(cur);
+       while(cur != null&& stack.isEmpty()){
+           Node temp =  stack.pop();
+      System.out.println(cur.val+" ");
+           cur = cur.leftChild;
+           cur = cur.rightChild;
+       }
+       while (!stack.isEmpty()){
+           
+       }
+    }
 
 
 

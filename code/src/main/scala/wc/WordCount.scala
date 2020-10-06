28f170e245c87385eff3c7297f9b1713002907a1
xido <1661480468@qq.com>
2020-10-06 19:28:26 +0800

    shucang

---
+++ code/src/main/scala/wc/WordCount.scala
@@ -0,0 +1,14 @@
+package wc
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+//批处理命令的wordcount
+//
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
